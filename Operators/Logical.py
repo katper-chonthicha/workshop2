dc0c33af92bd849e3f2882eeff908416fe9963b4
katper-chonthicha <chonthicha.sit@ku.th>
2021-02-06 23:07:46 +0700

    no message

---
+++ Operators/Logical.py
@@ -0,0 +1,8 @@
+x = True
+y = False
+
+print("x and y is", x and y) #ใช้ในกรณีที่ถูกทั้งหมด
+
+print("x or y is", x or y)
+
+print("not x is", not x)
---
+++ Operators/Membership.py
@@ -0,0 +1,27 @@
+# EX1
+string = "Hello world"
+
+print("H" in string)
+
+print("hELLO" not in string)
+
+# EX2
+dictObject = {"name": "Jim & Por","age": "26"}
+
+print("name" in dictObject)
+
+print("Jim & Por" in dictObject)
+
+# Ex3
+number_list = [1,2,3]
+
+print(1 in number_list)
+
+print(5 in number_list)
+
+# EX4
+name_list = ["jim","por"]
+
+print("jim" in name_list)
+
+print("korn" in name_list)
